6426752c49ac35d53a3317344e1404d8b73ba0ef
ninypops <ninypopsgame@gmail.com>
2022-03-30 10:54:07 +0100

    Store current question in state obj to manipluate quiz index as user clicks answers

--- src/components/quiz.js
+++ src/components/quiz.js
@@ -1,33 +1,45 @@
-import React from 'react';
+import React , { useState } from 'react';
+// import { Button, Carousel } from 'react-bootstrap';
 import quizObj from '../constants';
 
-const Quiz = () => (
-    <>
-        {
-            quizObj.map((q, i) => (
-                // TODO: Wrap in carousel
-                <>
-                    <p key={i}>
-                        {q.question}
-                    </p>
-                    <ul className="test">
-                        {(q.answers).map((a, i) => (
-                            <li key={`${i}_answers`}>
-                                {a}
-                            </li>
-                        ))}
-                    </ul>
-                    <button
-                        type="button"
-                        disabled=""
-                        onClick={() => {console.log('button is clicked')}}
-                    >
-                        Check answer
-                    </button>
-                </>
-            ))
+const Quiz = () => {
+    const [currentQuestion, setCurrentQuestion] = useState(0);
+
+    // Btn click takes user to next index in obj
+    const handleAnswerButtonClick = (answers) => {
+        const nextQuestion = currentQuestion + 1;
+        // If next question is less than total no. of questions, update state to next question
+        if (nextQuestion < quizObj.length) {
+            setCurrentQuestion(nextQuestion);
+        } else {
+            // else, add alert when user reaches obj limit so we don't error out
+            alert(`You've reached the end of the quiz!`);
         }
-    </>
-)
+    }
+
+    return (
+        <div
+            className="quiz"
+        >
+            <>
+                <p className="quiz__question">
+                    {quizObj[currentQuestion].question}
+                </p>
+                <div className="quiz__answer-section">
+                    {(quizObj[currentQuestion].answers).map((a, i) => (
+                        <button
+                            key={`${i}_answers`}
+                            type="submit"
+                            disabled=""
+                            onClick={() => {handleAnswerButtonClick()}}
+                        >
+                            {a}
+                        </button>
+                    ))}
+                </div>
+            </>
+        </div>
+    )
+}
 
 export default Quiz;
